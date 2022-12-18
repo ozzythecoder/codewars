ec808ec08eb80dcd08dc5bd539c19c60cd85ad1e
August McAllister <august.chm@gmail.com>
2022-12-18 13:13:43 -0600

    Added files & kata for 12.18

---
+++ 12.18/code.js
@@ -0,0 +1,19 @@
+const crowd = [
+  "                              ",           // Air
+  "                              ",           // Air
+  "            _                 ",           // Top of pyramid
+  "          _____               ",           // Layer of pyramid
+  "        _________             ",           // Layer of pyramid
+  "  B  _______________   G   GG "            // Ground layer of pyramid with several people, including Waldo
+];
+
+function findWaldoCoordinates(crowd) {
+
+  
+
+  return 'yes';
+}
+
+console.log(
+  findWaldoCoordinates(crowd)
+)
