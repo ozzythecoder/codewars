e66cf2c413b073c351f4f1dafd68fc0cf0e2c8c4
August McAllister <august.chm@gmail.com>
2022-12-24 09:30:55 -0600

    Completed kata for 12.24

--- 12.24/code.js
+++ 12.24/code.js
@@ -1,8 +1,16 @@
+// This one hurt my brain. Tried working it out with loops,
+// but regex ended up feeling like the easiest way.
+
 function reverseWords(str) {
   
-  return str;
+  // Get string as an array of words and spaces
+  return str.match( /(\s|[^\s]+)/g ).map(el => {
+    // return each element as a space if it is a space,
+    // or as a reversed word, if it is a word
+    return el.match( /\s/g ) ? el : el.split('').reverse().join('')
+  }) // then join it all together.
+    .join('')
 }
-
 
 console.log(
   reverseWords('This is an example!')
---
+++ 12.24/readme.md
@@ -0,0 +1,3 @@
+## [Reverse Words](https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/)
+
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
